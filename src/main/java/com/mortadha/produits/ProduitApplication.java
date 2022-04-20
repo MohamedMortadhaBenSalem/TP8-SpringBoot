355e3a59ea9ab1b2fd9e5a0c7b2c9b8cee951be2
MohamedMortadhaBenSalem <mortadha19531@gmail.com>
2022-04-20 16:00:00 +0100

    TP5 - SpringBoot

--- src/main/java/com/mortadha/produits/ProduitApplication.java
+++ src/main/java/com/mortadha/produits/ProduitApplication.java
@@ -6,20 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import com.mortadha.produits.entities.Produit;
 import com.mortadha.produits.service.ProduitService;
 
 @SpringBootApplication
 public class ProduitApplication implements CommandLineRunner {
-@Autowired
-ProduitService produitService;
-public static void main(String[] args) {
-SpringApplication.run(ProduitApplication.class, args);
+	@Autowired
+	private RepositoryRestConfiguration repositoryRestConfiguration;
+	public static void main(String[] args) {
+	SpringApplication.run(ProduitApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+	repositoryRestConfiguration.exposeIdsFor(Produit.class);
+	}
 }
-@Override
-public void run(String... args) throws Exception {
-produitService.saveProduit(new Produit("PC Dell", 2600.0, new Date()));
-produitService.saveProduit(new Produit("PC Asus", 2800.0, new Date()));
-produitService.saveProduit(new Produit("Imprimante Epson", 900.0, new Date()));
-}}
